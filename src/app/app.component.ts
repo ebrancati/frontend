5a91d3e75668ba37084a462834b2d24d86a501ca
EnzoBrancati <enzobrancati28@gmail.com>
2025-05-05 23:01:15 +0200

    V2.5 add header, footer and content components. Implemented piece movement, promotion to king and multiple captures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import {BoardComponent} from '../component/board/board.component';
+import {HeaderComponent} from '../app/components/header/header.component';
+import {BoardComponent} from '../app/components/board/board.component';
+import { ContentComponent } from "./components/content/content.component";
+import {FooterComponent} from '../app/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [BoardComponent],
+  imports: [HeaderComponent, BoardComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
