c12112b607a28a5321bb924d895cdca0f0169cce
EnzoBrancati <enzobrancati28@gmail.com>
2025-05-09 05:04:15 +0200

    implement multilingual support and fix moves display

--- src/app/pages/rules-content/rules-content.page.ts
+++ src/app/pages/rules-content/rules-content.page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'page-rules-content',
   standalone: true,
+  imports: [TranslateModule],
   templateUrl: './rules-content.page.html',
   styleUrl: './rules-content.page.css'
 })
--- src/model/entities/GameResponse.ts
+++ src/model/entities/GameResponse.ts
@@ -1,18 +1,20 @@
-interface GameResponse {
+export interface PlayerDto {
+  id: string;
+  nickname: string;
+  team: 'WHITE' | 'BLACK';
+}
+
+export interface GameResponse {
+  chat: string;
   id: string;
   board: string[][];
-  turno: string;  // "WHITE" or "BLACK"
+  turno: 'WHITE' | 'BLACK' | 'NONE';
   pedineW: number;
   pedineB: number;
   damaW: number;
   damaB: number;
   partitaTerminata: boolean;
-  vincitore: string;  // "NONE", "WHITE", or "BLACK"
-  chat: string;
-  players:
-    {
-    id: string;
-    nickname: string;
-    team: string;  // "WHITE" or "BLACK"
-  }[];
+  vincitore: 'WHITE' | 'BLACK' | 'NONE';
+  players: PlayerDto[];
+  cronologiaMosse: string[];
 }
