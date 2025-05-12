5b7f3452673c022696313819268ff83643433f6b
danifila <daniele.filareti@icloud.com>
2025-05-12 10:44:12 +0200

    V7.5

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,42 +1,38 @@
-  import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
-  import { FormsModule } from '@angular/forms';
-  import { CommonModule } from '@angular/common';
-  import { GameService } from '../../../services/game.service';
-  import { TranslateModule } from '@ngx-translate/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { GameService } from '../../../services/game.service';
+import { TranslateModule } from '@ngx-translate/core';
 
-  @Component({
-    selector: 'app-chat',
-    standalone: true,
-    imports: [CommonModule, FormsModule, TranslateModule],
-    templateUrl: './chat.component.html',
-    styleUrl: './chat.component.css'
-  })
-  export class ChatComponent {
-    @Input() gameId!: string;
-    @Input() chatHistory: string = '';
+@Component({
+  selector: 'app-chat',
+  standalone: true,
+  imports: [CommonModule, FormsModule, TranslateModule],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.css'
+})
+export class ChatComponent {
+  @Input() gameId!: string;
+  @Input() chatHistory: string = '';
 
+  messageInput: string = '';
+  @Input() nickname!: string | null;
 
-    messageInput: string = '';
-    @ViewChild('chatContainer') chatContainer!: ElementRef;
-    @Input() nickname: string = '';
+  constructor(private gameService: GameService) {}
 
+  sendMessage(): void {
+    const text = this.messageInput.trim();
+    if (!text) return;
 
-    constructor(private gameService: GameService) {
-    }
+    const payload = { player: this.nickname, text };
 
-    sendMessage(): void {
-      const text = this.messageInput.trim();
-      if (!text) return;
-
-      const payload = {player: this.nickname, text};
-
-      this.gameService.sendMessages(this.gameId, payload).subscribe({
-        next: () => {
-          this.messageInput = '';
-        },
-        error: err => console.error('Error in chat component', err)
-      });
-    }
+    this.gameService.sendMessages(this.gameId, payload).subscribe({
+      next: () => {
+        this.messageInput = '';
+      },
+      error: err => console.error('Popi popi in chat component ts', err)
+    });
+  }
 
 
 
